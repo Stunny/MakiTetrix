933d66eed7bde8430426eb3e235ef6a186d13a84
Alex Vogel <avogelm@gmail.com>
2017-04-04 09:32:35 +0200

    CLIENT: LoginController.java: created with main functionalities -- User.java: created with main fields -- UserAccessRepository.java: interface that defines login and register behavior

---
+++ App/src/model/User.java
@@ -0,0 +1,36 @@
+package model;
+
+/**
+ * Defines a Tetrix User and all his data
+ * Created by avoge on 04/04/2017.
+ */
+public class User {
+
+    private String userName;
+
+    private String password;
+
+    public User() {
+    }
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
