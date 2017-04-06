4e370fcb655186915ca60ade6e0cc3e9dfaa5976
miquelator <miquelet-sans@hotmail.com>
2017-04-06 11:17:47 +0200

    merge versions base de dades

--- App/Server/src/Model/GestioDades.java
+++ App/Server/src/Model/GestioDades.java
@@ -1,13 +1,11 @@
 package Model;
-
+import java.sql.*;
 import Controller.ServerController;
 import java.io.DataInputStream;
 import java.util.ArrayList;
 
 
-/**
- * Created by Admin on 24/03/2017.
- */
+
 public class GestioDades {
     //tractament de dades --> info del server a la BBDD (Miquel)
     public GestioDades(){
