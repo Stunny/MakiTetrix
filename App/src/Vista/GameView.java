d1100fcaa17bed38d3f59604d13f662e6dd7de5e
miquelator <miquelet-sans@hotmail.com>
2017-04-23 15:11:35 +0200

    Empezada la ventana de juego

--- App/src/Vista/GameView.java
+++ App/src/Vista/GameView.java
@@ -11,20 +11,20 @@ import java.awt.*;
  */
 public class GameView extends JFrame{
 
-    private JLabel[][]caselles;
+    private JPanel[][]caselles;
     private JPanel centre;
 
     public GameView(){
         centre = new JPanel(new GridLayout(25,10));
         centre.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
         getContentPane().add(centre);
-        setSize(550,460);
-         caselles = new JPanel[][]();
+        setSize(550,1260);
+         caselles = new JPanel[25][10];
 
         for (int i = 0; i < 25; i++) {
-            for (int j = 0; j < 10; i++) {
-                JLabel p = new JLabel();
-                caselles[i][j] =p;
+            for (int j = 0; j < 10; j++) {
+                JPanel p = new JPanel();
+                caselles[i][j] = p;
                 caselles[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK));
             }
         }
