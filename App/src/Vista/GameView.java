c6834f286d2e708ca2c75659d30a5fbd2086727b
zitro69 <jortizc96@gmail.com>
2017-04-25 11:18:35 +0200

    GameView: barra info arriba creada y terminada (faltan gettes y setters), siguiente pieza creado. - Creadas las distintas clases de las piezas.

--- App/src/Vista/GameView.java
+++ App/src/Vista/GameView.java
@@ -1,24 +1,27 @@
 package Vista;
 
-import controller.RegisterController;
-
 import javax.swing.*;
 import java.awt.*;
 
 /**
- * Ventana principal de registro del cliente
+ * Ventana principal del juego.
  * Created by jorti on 31/03/2017.
  */
 public class GameView extends JFrame{
 
     private JPanel[][]caselles;
     private JPanel centre;
+    private JLabel temps;
+    private JLabel nivel;
+    private JLabel puntuacion;
+    private JPanel siguientePieza;
 
     public GameView(){
+
+        //Pantalla principal del juego
         centre = new JPanel(new GridLayout(25,10));
         centre.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
-        getContentPane().add(centre);
-        setSize(550,1260);
+        setSize(800,1260);
         caselles = new JPanel[25][10];
 
         for (int i = 0; i < 25; i++) {
@@ -30,19 +33,37 @@ public class GameView extends JFrame{
                 centre.add(p);
             }
         }
-        getContentPane().add(centre);
+        getContentPane().add(centre, BorderLayout.CENTER);
+
+        //Información necesaria en la parte de arriba del nivel
+
+        temps = new JLabel ("XX:XX");
+        nivel = new JLabel ("Nivel X");
+        puntuacion = new JLabel ("0000 - eres inútil");
+
+        JPanel north = new JPanel (new FlowLayout());
+        north.add(temps);
+        north.add(nivel);
+        north.add(puntuacion);
+
+        getContentPane().add(north, BorderLayout.NORTH);
+
+        //En la parte de arriba irá la siguiente figura, en la zona de abajo se verán la gente que te observa.
+        JPanel east = new JPanel(new GridLayout(2,1));
+
+        siguientePieza = new JPanel(new GridLayout(4,4));
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 4; j++){
+                JPanel p = new JPanel();
+                p.setBackground(Color.blue);
+                p.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                siguientePieza.add(p);
+            }
+        }
+        east.add(siguientePieza);
+
+        getContentPane().add(east,BorderLayout.EAST);
 
     }
-
-    /**
-     *
-     * @param controller
-     */
-    public void registerController(RegisterController controller){
-
-
-
-    }
-
 
 }
---
+++ App/src/model/Piezas/EleInvertida.java
@@ -0,0 +1,7 @@
+package model.Piezas;
+
+/**
+ * Created by jorti on 25/04/2017.
+ */
+public class EleInvertida {
+}
