3995537393a93830c78dd91011c1971241b57131
Pedro Riera <rieramanchon@gmail.com>
2017-05-10 18:56:22 +0200

    utils.Encrypter: Acabado

--- App/src/model/utils/Encrypter.java
+++ App/src/model/utils/Encrypter.java
@@ -1,6 +1,14 @@
 package model.utils;
 
+import org.apache.commons.crypto.cipher.CryptoCipher;
+import org.apache.commons.crypto.cipher.CryptoCipherFactory;
+import org.apache.commons.crypto.utils.Utils;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 
 /**
  * Created by pedroriera on 6/4/17.
@@ -11,7 +19,7 @@ public class Encrypter {
      * @param mail string que contiene el mail
      * @param pass string que contiene el pass
      */
-    public void encryptLogin(String mail, String pass) {
+    public void encryptLogin(String mail, String pass) throws Exception {
         encryptMail(mail);
         encryptPass(pass);
     }
@@ -22,7 +30,7 @@ public class Encrypter {
      * @param pass string  que contine el pass
      * @param user string que contiene el user
      */
-    public void encryptSingUp(String mail, String pass, String user) {
+    public void encryptSingUp(String mail, String pass, String user) throws Exception {
         encryptMail(mail);
         encryptPass(pass);
         encryptUserName(user);
@@ -33,8 +41,9 @@ public class Encrypter {
      * @param user string que contiene el nombre de usuario
      * @return contiene un array de bits con el nombre de usuario encriptado
      */
-    private byte[] encryptUserName(String user) {
-        return user.getBytes(StandardCharsets.UTF_8);
+    private byte[] encryptUserName(String user) throws Exception {
+        byte[] input = user.getBytes(StandardCharsets.UTF_8);
+        return encyptBytes(input);
 
     }
 
@@ -43,8 +52,9 @@ public class Encrypter {
      * @param mail string que contiene el mail
      * @return contiene un array de bits con el mail encriptado
      */
-    private byte[] encryptMail(String mail) {
-        return mail.getBytes(StandardCharsets.UTF_8);
+    private byte[] encryptMail(String mail) throws Exception {
+        byte[] input = mail.getBytes(StandardCharsets.UTF_8);
+        return encyptBytes(input);
 
     }
 
@@ -53,15 +63,19 @@ public class Encrypter {
      * @param pass string que contiene la contrasena
      * @return contiene un array de bits con la contrasena encriptada
      */
-    private byte[] encryptPass(String pass) {
-        byte[] b = pass.getBytes(StandardCharsets.UTF_8);
-        //hacer una opearacion maxi chunga y secreta para encriptar
-        //b = b;
-       return b;
+    private byte[] encryptPass(String pass) throws Exception {
+        byte[] input = pass.getBytes(StandardCharsets.UTF_8);
+        return encyptBytes(input);
 
     }
 
-    /*private byte[] encyptBytes(byte[] input) {
+    /**
+     * Encripta una cadena de bytes
+     * @param input cadena de bytes a encriptar
+     * @return cadena de bytes encriptada
+     * @throws Exception Excepcion de seguridad
+     */
+    private byte[] encyptBytes(byte[] input) throws Exception {
         final SecretKeySpec key = new SecretKeySpec(getUTF8Bytes("1234567890123456"), "AES");
         final IvParameterSpec iv = new IvParameterSpec(getUTF8Bytes("1234567890123456"));
 
@@ -71,8 +85,27 @@ public class Encrypter {
         final String transform = "AES/CBC/PKCS5Padding";
         CryptoCipher encipher = Utils.getCipherInstance(transform, properties);
 
-    }*/
+        byte[] output = new byte[32];
+        //Initializes the cipher with ENCRYPT_MODE, key and iv.
+        encipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        //Continues a multiple-part encryption/decryption operation for byte array.
+        int updateBytes = encipher.update(input, 0, input.length, output, 0);
+        System.out.println(updateBytes);
+        //We must call doFinal at the end of encryption/decryption.
+        int finalBytes = encipher.doFinal(input, 0, 0, output, updateBytes);
+        System.out.println(finalBytes);
+        //Closes the cipher.
+        encipher.close();
 
+        return output;
+
+    }
+
+    /**
+     * Convierte un string a cadena de bytes
+     * @param input String a convertir
+     * @return Cadena de bytes que contiene los datos del input en forma de bytes
+     */
     private static byte[] getUTF8Bytes(String input) {
         return input.getBytes(StandardCharsets.UTF_8);
     }
